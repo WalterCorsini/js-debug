900e40e6b5540fffc188088a1270d56a2c959aeb
WalterCorsini <corsini.walter@gmail.com>
2024-04-16 19:51:15 +0200

    add correction bundle-02 esercizio 5

--- js-debug/bundle-02/main.js
+++ js-debug/bundle-02/main.js
@@ -11,38 +11,38 @@
 
 
 // ESERCIZIO 1
-// function checkAge() {
-//     const myAge = 32;
-//     let message = '';
+function checkAge() {
+    const myAge = 32;
+    let message = '';
 
-//     if (myAge < 18) {
-//         console.log(`Sei troppo giovane! Hai ${myAge} anni!`);
-//     } else {
-//         console.log('Hai più di 18 anni!');
-//     }
-// }
-// checkAge();
+    if (myAge < 18) {
+        console.log(`Sei troppo giovane! Hai ${myAge} anni!`);
+    } else {
+        console.log('Hai più di 18 anni!');
+    }
+}
+checkAge();
 // errore riga 16 la variabile non puo essere dichiarata con const perche il suo valore cambia
 // erorre la funzione dovrebbe restituire la variabile message con un return e a sua volta la funzione invocata dovrebbe essere salvata in una variabile o dentro la condizione dovrebbero esserci dei console.log e quindi la variabile message non avrebbe senso.
 
 
 // ESERCIZIO 2
-// function printColorsNumber() {
-//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-//     console.log(`Nella mia palette ci sono ${colors.length} colori!`);
-// }
-// printColorsNumber();
+function printColorsNumber() {
+    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
+}
+printColorsNumber();
 // errore di sintassi nella riga 32 "length" e non "lenght"
 
 
 // ESERCIZIO 3
-// function addNumbers() {
-//     const userNumber = parseInt(prompt('Inserisci un numero'));
-//     const total = userNumber + 12;
+function addNumbers() {
+    const userNumber = parseInt(prompt('Inserisci un numero'));
+    const total = userNumber + 12;
 
-//     console.log(`Il risultato finale è ${total}`);
-// }
-// addNumbers();
+    console.log(`Il risultato finale è ${total}`);
+}
+addNumbers();
 // errore riga 40 assicurandoci che il nostro utente inserisca un numero senza fare troppi controlli prima del prompt che resituisce delle stringe,dobbiamo passargli la funzione "parseInt"
 
 // ESERCIZIO 4
@@ -68,7 +68,7 @@ checkAccess();
 // controlla se l'email è presente nell'array
 
 
-// // ESERCIZIO 5 (suggerimento: c'è un solo errore)
+// ESERCIZIO 5 (suggerimento: c'è un solo errore)
 // function checkAccessImproved() {
 //     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
@@ -94,9 +94,42 @@ checkAccess();
 //             console.log('Accesso negato!');
 //         }
 //     }
+    
 //     checkAccessImproved();
+// ci sono piu errori
+// errore riga 97 manca la graffa di chiusura della funzione
+// errore controllo di accesso o meno deve stare fuori dal ciclo for
+// errore variabile true o false deve essere dichiarata con apici
+// errore riga 85 alla variabile non gli deve essere assegnato il valore con apici ma senza
+// se vogliamo garantire un controllo tale che l'utente puo inserire anche delle lettere maiuscole e visualizzare l'email come corretta, nel controllo alla variabile userEmail aggiungiamo la funzione .toLowerCase() per trasformare tutti i caratteri in minuscolo
 
+//                                                  /
+//                 ESERCIZIO 5 CORRETTO             /
+//                                                  /
+function checkAccessImproved() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+    let grantAccess = false;
+
+    for (let i = 0; i < addresses.length; i++) {
+        const email = addresses[i];
+    
+        if (userEmail.length > 5) {
+            if (email === userEmail.toLowerCase()) {
+                grantAccess = true;
+            }
+        }
+    }
+
+    if (grantAccess) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+    checkAccessImproved();
 
 
 
